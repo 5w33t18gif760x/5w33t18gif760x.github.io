48246b057520f9fa2c2038748827c2a8320964bc
5w33t18gif760x <39699064+5w33t18gif760x@users.noreply.github.com>
2018-07-15 04:48:05 +0600

    Delete function.js

--- js/function.js
+++
@@ -1,9 +0,0 @@
-$('.menu-btn').on('click', function(e) {
-  e.preventDefault();
-  $('.menu').toggleClass('menu_active');
-  $('.wrapper').toggleClass('wrapper_active');
-})
-
-$('.card').on('click', function() {
-    $('.container').toggleClass('container-origin');
-})
